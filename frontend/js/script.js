75004bf8c4bd891b5ec2c59186f4a31490e5b84f
mellek89 <melaniekoester89@gmail.com>
2026-05-13 07:40:37 -0300

    coole animation

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -14,105 +14,146 @@ document.querySelectorAll('.lang').forEach(el => {
     window.location.href = `/${lang}/${file}`;
   });
 });
+const tl = gsap.timeline();
 
- const tl = gsap.timeline();
+// Initial States
+gsap.set(".svgLogoIntro", {
+  opacity: 0,
+  scale: 0.9
+});
 
 gsap.set(".brace-left", {
-  x:-120,
-  opacity:0,
-  rotate:-8
+  x: -120,
+  opacity: 0,
+  rotate: -8
 });
 
 gsap.set(".brace-right", {
-  x:120,
-  opacity:0,
-  rotate:8
+  x: 120,
+  opacity: 0,
+  rotate: 8
 });
 
 gsap.set(".m-red", {
-  scale:0,
-  opacity:0,
-   transformOrigin:"50% 50%"
+  scale: 0,
+  opacity: 0,
+  transformOrigin: "50% 50%"
 });
 
 gsap.set(".logo-text", {
-  y:10,
-  opacity:0,
-  filter:"blur(8px)"
+  y: 10,
+  opacity: 0,
+  filter: "blur(8px)"
 });
 
 
 // TIMELINE
-
 tl
 
-// Klammern rein
-.to(".brace-left",{
-  x:0,
-  opacity:1,
-  rotate:0,
-  duration:1.2,
-  ease:"expo.out"
+// "Web it!" erscheint
+.from(".webit", {
+  opacity: 0,
+  y: 20,
+  duration: 1
 })
 
-.to(".brace-right",{
-  x:0,
-  opacity:1,
-  rotate:0,
-  duration:1.2,
-  ease:"expo.out"
-},"<")
+// "it!" verschwindet
+.to(".it", {
+  opacity: 0,
+  x: 40,
+  duration: 0.7,
+  ease: "power2.out"
+})
+
+// "Web" nach links
+.to(".web", {
+  x: -60,
+  duration: 1,
+  ease: "power3.inOut"
+}, "-=0.3")
+
+// Logo erscheint
+.to(".svgLogoIntro", {
+  opacity: 1,
+  scale: 1,
+  duration: 1
+}, "-=0.6")
+
+// Klammern rein
+.to(".brace-left", {
+  x: 0,
+  opacity: 1,
+  rotate: 0,
+  duration: 1.2,
+  ease: "expo.out"
+})
+
+.to(".brace-right", {
+  x: 0,
+  opacity: 1,
+  rotate: 0,
+  duration: 1.2,
+  ease: "expo.out"
+}, "<")
 
 // M erscheint
-.to(".m-red",{
-  scale:1,
-  opacity:1,
-  duration:1,
-  ease:"elastic.out(1,0.5)"
-},"-=0.4")
+.to(".m-red", {
+  scale: 1,
+  opacity: 1,
+  duration: 1,
+  ease: "elastic.out(1,0.5)"
+}, "-=0.4")
 
 // Text smooth rein
-.to(".logo-text",{
-  y:0,
-  opacity:1,
-  filter:"blur(0px)",
-  duration:1,
-  stagger:0.08,
-  ease:"power3.out"
-},"-=0.5")
+.to(".logo-text", {
+  y: 0,
+  opacity: 1,
+  filter: "blur(0px)",
+  duration: 1,
+  stagger: 0.08,
+  ease: "power3.out"
+}, "-=0.5")
 
 // leichter cinematic zoom
-.to(".svgLogoIntro",{
-  scale:1.03,
-  duration:0.6,
-  ease:"power2.out"
+.to(".svgLogoIntro", {
+  scale: 1.03,
+  duration: 0.6,
+  ease: "power2.out"
 })
 
-.to(".svgLogoIntro",{
-  scale:1,
-  duration:1,
-  ease:"elastic.out(1,0.4)"
+.to(".svgLogoIntro", {
+  scale: 1,
+  duration: 1,
+  ease: "elastic.out(1,0.4)"
 })
 
-// Pause
-.to({}, {duration:0.5})
+// kurze Pause
+.to({}, {
+  duration: 0.5
+})
 
-// Intro raus
-.to("#intro",{
-  opacity:0,
-  duration:1,
-  ease:"power2.inOut"
+// Intro ausblenden
+.to("#intro", {
+  opacity: 0,
+  duration: 1,
+  ease: "power2.inOut"
 })
 
 // entfernen
-.set("#intro",{
-  display:"none"
+.set("#intro", {
+  display: "none"
 })
 
-// Content rein
-.to("#content",{
-  opacity:1,
-  y:0,
-  duration:1.2,
-  ease:"power3.out"
-});
+// Content einblenden
+.fromTo("#content",
+  {
+    opacity: 0,
+    y: 30
+  },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 1.2,
+    ease: "power3.out"
+  }
+);
